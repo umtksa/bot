a7c1aa9398c98f14ad2a544dc63f01001c827cd7
umtksa <umtksa@gmail.com>
2025-05-09 14:21:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -167,6 +167,9 @@ function getBotResponse(userInput) {
     if (["teşekkür ederim", "teşekkürler", "tenks", "thanks", "sağ ol", "sağol", "çok teşekkürler"].some(word => lowerInput.includes(word))) {
         return "Rica ederim!";
     }
+    if (["adın ne", "ismin"].some(word => lowerInput.includes(word))) {
+        return "adım ahraz.";
+    }
     if (["mal mısın", "gerizekalı", "salak", "aptal"].some(word => lowerInput.includes(word))) {
         return "oldukça gerizekalıyım";
     }
