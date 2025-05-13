13e832f87c2e449d075fb27ec90c98f7db31da67
umtksa <umtksa@gmail.com>
2025-05-13 19:05:48 +0300

    deneme

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let ocrWorker;
 // Bu liste, metin işlenirken çıkarılacak yaygın kelimeleri içerir.
 // Fuse.js kendi algoritmalarını kullanabilir, ancak arama terimini temizlerken kullanışlı olabilir.
 const turkishStopwords = new Set([
-    "nedir", "kaçtır", "ne", "kaç","neresidir"
+    "nedir", "kaçtır", "ne", "kaç", "neresidir", "kodu", "numarası"
     // Daha fazla stopword ekleyebilirsin
 ]);
 // --- Stopword Listesi Sonu ---
